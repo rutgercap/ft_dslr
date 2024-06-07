753f2a59398994ba78df5505a8aa82a9ab876c35
Rutger Cappendijk <rutgercappendijk@gmail.com>
2024-06-07 14:08:07 +0200

    added first test

---
+++ src/describe.py
@@ -0,0 +1,8 @@
+def describe() -> int:
+    return 42
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_describe.py
@@ -0,0 +1,4 @@
+from src.describe import describe
+
+def test_can_run_describe() -> None:
+    assert describe() == 42
