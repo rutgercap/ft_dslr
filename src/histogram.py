6ca6d648467b1ce1eb1ae6ccf3c70eecf3d17fb0
yoonsele <yoonseo9800@gmail.com>
2024-07-22 17:09:10 +0200

    figuring legend

--- src/histogram.py
+++ src/histogram.py
@@ -1,5 +1,7 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
+from math import floor
 
 def read_file(path_to_training_data: str) -> pd.DataFrame:
     try:
@@ -11,8 +13,39 @@ def read_file(path_to_training_data: str) -> pd.DataFrame:
     return df
 
 def draw_histogram(dataframe: pd.DataFrame):
-    dataframe.hist(bins=4)
-    print(dataframe.index)
+    columns = dataframe.columns.to_list()
+    for items in ['Index', 'Hogwarts House', 'First Name', 'Last Name', 'Birthday','Best Hand']:
+        columns.remove(items)
+    # print(len(columns))
+
+
+    figure, axis = plt.subplots(4, 4)
+
+    plt.subplots_adjust(hspace=0.5)
+
+    for i, subject in enumerate(columns):
+        H = dataframe.loc[dataframe['Hogwarts House'] == 'Hufflepuff', subject]
+        G = dataframe.loc[dataframe['Hogwarts House'] == 'Gryffindor', subject]
+        R = dataframe.loc[dataframe['Hogwarts House'] == 'Ravenclaw', subject]
+        S = dataframe.loc[dataframe['Hogwarts House'] == 'Slytherin', subject]
+
+        axis[floor(i/4), i%4].hist(H,color='r', alpha=0.5, label= 'Hufflepuff', bins=100)
+        axis[floor(i/4), i%4].hist(G,color='g', alpha=0.5, label= 'Gryffindor', bins=100)
+        axis[floor(i/4), i%4].hist(R,color='b', alpha=0.5, label= 'Ravenclaw', bins=100)
+        axis[floor(i/4), i%4].hist(S,color='y', alpha=0.5, label= 'Slytherin', bins=100)
+
+        axis[floor(i/4), i%4].set_title(f'{subject} Distribution by House')
+        axis[floor(i/4), i%4].set_xlabel(subject)
+        axis[floor(i/4), i%4].set_ylabel('Frequency')
+
+    
+    # figure.legend([l1, l2], labels=labels, loc="upper right") 
+    # leg = ax.get_legend()
+    # leg.legend_handles[0].set_color('red')
+    # leg.legendHandles[1].set_color('yellow')
+    # leg = plt.legend(['Hufflepuff', 'Gryffindor', 'Ravenclaw', 'Slytherin'], title='House')
+    plt.show()
+
 
 def main():
     args = sys.argv[1:]
