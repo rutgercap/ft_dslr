ad961833ccb67841dd0353c5ef3bc5285531ca33
rutger <rutgercappendijk@gmail.com>
2024-08-08 11:16:23 +0200

    implemented logistic regression
    Co-authored-by: yoonsele <christie26@users.noreply.github.com>

---
+++ src/logistic_regression.py
@@ -0,0 +1,41 @@
+from typing import Sequence
+from numpy import ndarray
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def prediction_loss(y_pred: ndarray, y: ndarray):
+    return -y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.001, epochs=1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X: ndarray, y: ndarray):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            linear_pred = np.dot(X, self.weights) + self.bias
+            predictions = sigmoid(linear_pred)
+            loss = np.mean(prediction_loss(predictions, y))
+            print("loss:", loss)
+            dw = (1 / n_samples) * np.dot(X.T, (predictions - y))
+            db = (1 / n_samples) * np.sum(predictions - y)
+
+            self.weights = self.weights - self.learning_rate * dw
+            self.bias = self.bias - self.learning_rate * db
+
+    def predict(self, X) -> Sequence[int]:  
+        linear_pred = np.dot(X, self.weights) + self.bias
+        y_pred = sigmoid(linear_pred)
+        class_pred = [0 if y <= 0.5 else 1 for y in y_pred]
+        return class_pred
+
--- src/logreg_train.py
+++ src/logreg_train.py
@@ -1,10 +1,8 @@
 import sys
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt  # type: ignore
+from logistic_regression import LogisticRegression
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import math
+
 
 def read_file(path_to_training_data: str) -> pd.DataFrame:
     try:
@@ -15,56 +13,34 @@ def read_file(path_to_training_data: str) -> pd.DataFrame:
         exit(1)
     return df
 
-def formula(values: list, weights: list) -> float:
-    if (len(values) +  1 != len(weights)):
-        raise ValueError("values and weights have to have same length.")
-    result = weights[len(values)]
-    for a, b in zip(values, weights):
-        result += a * b
-    return result
 
-def continuous_to_categorical(df):
-    pd.cut(df.results,bins=[0,1,2,3,4],labels=['Hufflepuff','Gryffindor','Ravenclaw','Slytherin'])
-
-# results = {
-#     'H' : 0.3
-#     'G' : 0.2
-#     'R' : 0.1
-#     'S' : 0.4 - correct
-# }
-# log(0.4) + log(1-0.3) + log(1-0.2) + log(1-0.1)
-def calculate_loss(results: dict, correct_house: str):
-    # y * log(h(x)) + (1-y) * log(1-h(x))
-    sum = 0
-    for key, value in results.items:
-        y = key == correct_house
-        sum += y * math.log(value) + (1 - y) * math.log(1 - value)
-    return sum
+def get_df(path_to_dataset: str):
+    df = read_file(path_to_dataset)
+    df.dropna(inplace=True)
+    df.drop(
+        columns=["Index", "First Name", "Last Name", "Birthday", "Best Hand"],
+        inplace=True,
+    )
+    return df
 
 
-def softmax(results):
-    return (np.exp(results)/sum(np.exp(results)))
+def get_X_and_Y(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
+    X = df.drop(columns=["Hogwarts House"])
 
-def one_hot_encoded_vector(k: str) -> list[int]:
-    # return [k == "Hufflepuff",k == "Gryffindor",k == "Ravenclaw",k == "Slytherin"]
-    return [0,0,0,0]
+    X = (X - X.mean()) / X.std()
+    Y = df["Hogwarts House"]
 
-def calculate_gredient(y_preds, X, Y, m):
-    X = X.reset_index(drop=True)
-    X = X.T
-    # X = X.reset_index(drop=True)
+    # delete it later!
+    X.drop(X.index[5:], inplace=True)
+    Y.drop(Y.index[5:], inplace=True)
 
-    gredients = []
-    for subject in X.iterrows():
-        i = 0
-        sum = 0
-        for y_pred, y in zip(y_preds, Y):
-            x = subject[1][i]
-            sum += y_pred[0] - (y == "Hufflepuff")
-            i += 1
-        gredient = sum / m * x
-        gredients.append(gredient)
-    return gredients
+    x, y = X.to_numpy(), Y.to_numpy()
+    return x, y
+
+
+def binary_vector(y: np.ndarray, correct_house: str) -> np.ndarray:
+    result = [1 if house == correct_house else 0 for house in y]
+    return np.array(result)
 
 
 def main():
@@ -76,35 +52,13 @@ def main():
     if not path_to_dataset.endswith(".csv"):
         print("Invalid file format. Only CSV files are supported.")
         exit(1)
-    df = read_file(path_to_dataset)
-    df.dropna(inplace=True)
-    df.drop(columns=['Index','First Name','Last Name','Birthday', 'Best Hand'], inplace=True)
-    X = df.drop(columns=['Hogwarts House'])
+    df_train = get_df(path_to_dataset)
+    X_train, y = get_X_and_Y(df_train)
 
-    X = (X-X.mean())/X.std()
-    Y = df['Hogwarts House']
-    m = Y.shape[0]
-
-    X.drop(X.index[5:], inplace=True)
-    Y.drop(Y.index[5:], inplace=True)
-
-    weights_per_house = {
-        house : np.array([np.random.random() for _ in range(len(X.columns) + 1)])
-        for house in ['Hufflepuff','Gryffindor','Ravenclaw','Slytherin']
-    }
-
-    for _ in range(1):
-        y_preds = []
-        for student in X.to_numpy():
-            results = {}
-            for house in weights_per_house:
-                results[house] = formula(student, weights_per_house[house])
-            y_pred = softmax(list(results.values()))
-            y_preds.append(y_pred)
-        gredients = calculate_gredient(y_preds, X, Y, m)
-        print(gredients)
-        # weight -= - gredient * learning_speed
-
+    y_Ravenclaw = binary_vector(y, "Ravenclaw")
+    clf = LogisticRegression(learning_rate=0.01)
+    clf.fit(X_train, y_Ravenclaw)
+    return 0
 
 if __name__ == "__main__":
     main()
